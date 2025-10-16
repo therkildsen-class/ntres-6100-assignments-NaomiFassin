7c031a65027dc2e4467030d459b58417b39d12c7
NaomiFassin <nf295@cornell.edu>
2025-10-16 12:26:46 -0400

    commit

--- course_notes/loop-exercises.R
+++ course_notes/loop-exercises.R
@@ -6,17 +6,19 @@ cntry <- "Afghanistan"
 country_list <- c("Albenia", "Canada", "Spain")
 
 dir.create("figures")
+dir.create("figures/europe")
 
 for (cntry in country_list) {
 
 gap_to_plot <- gapminder |> 
   filter(country == cntry)
 
-my_plot <- ggplot(data = gap_to_plot, mapping = aes(x = year, y = gdp_per_cap)) +
+my_plot <- ggplot(data = gap_to_plot, mapping = aes(x = year, y = gdp_tot)) +
   geom_point() +
-  labs(title = str_c(cntry, "GDP per cap", sep = " "))
+  labs(title = str_c(cntry, "GDP", sep = " "))
 
 
-ggsave(filename = str_c(cntry, "_gdp_per_cap.png", sep = ""), plot = my_plot)
+ggsave(filename = str_c("figures/europe/", cntry, "_gdp_per_cap.png", sep = ""), plot = my_plot)
 
 }
+
